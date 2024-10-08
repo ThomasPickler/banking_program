7f3ba2c1613e1cee7b234181dc7128ec798c4bd6
Thomas Pickler <107810480+Brokolizz@users.noreply.github.com>
2024-10-08 11:17:58 -0300

    Create banking_program.py

---
+++ banking_program.py
@@ -0,0 +1,28 @@
+
+
+
+def show_balance():
+    pass
+
+
+def deposit():
+    pass
+
+
+def withdraw():
+    pass
+
+
+balance = 0
+is_running = True
+
+while is_running:
+    print('Bank of Python')
+    print('1. Show Balance')
+    print('2. Deposit')
+    print('3. Withdraw')
+    print('4. Exit')
+
+    choice = input('Enter a number (1-4): ')
+
+    if choice
